009873a2bc637199bef461434204d855bc4997c3
Nishant-Burnwal <nishantburnwal52@gmail.com>
2025-07-12 13:02:26 +0530

    feat: Added volume of sphere with radius_cm of cirlce

--- circumference_and_area_of_circle.py
+++ circumference_and_area_of_circle.py
@@ -13,7 +13,9 @@ radius_cm = radius_inch * 2.54
 # Compute circumference and area
 circumference_cm = 2 * PI * radius_cm
 area_square_cm = PI * (radius_cm ** 2)
+volume_cube_cm = PI * (radius_cm ** 3)
 
 # Print the radius with new line separator
 print(f"\nCircumference of the Circle is {round(circumference_cm, 2)} cm and",
-      f"Area of the Circle is {round(area_square_cm, 2)} sqcm", sep="\n")
+      f"Area of the Circle is {round(area_square_cm, 2)} sqcm",
+      f"Volume of the Circle is {round(volume_cube_cm, 2)} sqcm", sep="\n")
