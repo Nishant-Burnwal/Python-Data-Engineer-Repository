3dc360e5d0656eddc3fe3d1063ba5812d64ab3ac
Nishant-Burnwal <nishantburnwal52@gmail.com>
2025-07-12 16:33:19 +0530

    fix: Added function for primelist

--- primes_list.py
+++ primes_list.py
@@ -10,21 +10,24 @@ def isPrime(n):
             if n % i == 0:
                 return False
         return True
-    
-# taking user input for range to check primes
-n1 = int(input("Enter the Range 1 number: "))
-n2 = int(input("Enter the Range 2 number: "))
-# creating a empty primes list
-primes = []
 
-for i in range(n1, n2 + 1):
-    if isPrime(i):
-        primes.append(i)
+def primelist():
+    # taking user input for range to check primes
+    n1 = int(input("Enter the Range 1 number: "))
+    n2 = int(input("Enter the Range 2 number: "))
+    # creating a empty primes list
+    primes = []
 
-if (len(primes) == 0):
-    print("No Prime number found.")
-else:
-    print(primes)
+    for i in range(n1, n2 + 1):
+        if isPrime(i):
+            primes.append(i)
+
+    if (len(primes) == 0):
+        print("No Prime number found.")
+    else:
+        print(primes)
+
+primelist()
 
     
         
