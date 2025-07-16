c6d5f08f43f959f7a69a5e39b2d9a2a079c971f8
Nishant-Burnwal <nishantburnwal52@gmail.com>
2025-07-16 18:43:26 +0530

    feat: Added code for string in python

---
+++ prog0_string_introduction.py
@@ -0,0 +1,21 @@
+#PROG: String Python 
+
+user_string = "hello world"
+
+print(user_string)
+
+print(user_string.title())
+
+print(len(user_string))
+
+print(user_string.upper())
+
+print(user_string.lower())
+
+print(user_string[0])
+
+print(user_string.count("l"))
+
+print(user_string.replace('world', 'universe'))
+
+print(help(str))
